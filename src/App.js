0badd1eee117f616596db614345d41129f154053
yawwusu <yawwusugh@gmail.com>
2021-05-11 21:47:17 +0200

    React router setup
    - Install react-router-dom
    - Added simple template for routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Let's Build Amazon</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
